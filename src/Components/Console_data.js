ff78538ec7dd7890224b6e264c9e829ba47a8d3c
Serge <lionheart.sergex@gmail.com>
2021-02-17 11:18:03 -0800

    corrected errors

--- src/Components/Console_data.js
+++ src/Components/Console_data.js
@@ -1,24 +1,24 @@
 export const ps1 =
 [
-    {text:`Final fantasy 7 launched for the original playstation in 1997. It was an RPG like no other and is one of the best selling RPG's for Square at the time.`, title:"Final Fantasy 7", adjustment:"ff7"},
+    {text:`Final fantasy 7 launched for the original PlayStation in 1997. It was an RPG like no other and is one of the best selling RPG's for Square at the time.`, title:"Final Fantasy 7", adjustment:"ff7"},
     {text:`This legendary sequel released in 1999. It sold a total of 9.37 million copies worldwide. The game became part of sony's greatest hits line.`, title:`Gran Turisimo series`, adjustment:"gt2"},
-    {text:`The sequel to the popular fighting game series released in 1998 for the Ps1. Tekken 3 is Playstation's  fourth best selling games.`, title:"Tekken 3", adjustment:"tekken3"},
+    {text:`The sequel to the popular fighting game series released in 1998 for the Ps1. Tekken 3 is PlayStation's  fourth best selling games.`, title:"Tekken 3", adjustment:"tekken3"},
     {text:`The start of the legendary stealth series was released in 1998. 7 million copies were sold worldwide.`, title:"Metal Gear Solid", adjustment:"mgs"},
     {text:`First big console survival horror game series launched in 1996. 4 million copies were sold worldwide. `, title:"Resident Evil", adjustment:"re1"},
 ];
 export const ps2 =
 [
-    {text:`First of the popular action game series released on the playstation 2 in 2001. 1 million copies were sold.`, title:"Devil May Cry", adjustment:"ps2standard"},
+    {text:`First of the popular action game series released on the PlayStation 2 in 2001. 1 million copies were sold.`, title:"Devil May Cry", adjustment:"ps2standard"},
     {text:`The start of the hit series was released in 2005 in japan followed in the U.S in 2006. The first game sold 1 million copies.`, title:"Yakuza", adjustment:"ps2standard"},
     {text:`This popular series was released in 2002. The game went on to sell 1.1 million copies.`, title:"Ratchet and Clank", adjustment:"ps2standard"},
     {text:`The hugely popular series begin its spike in popularity in the third installment released in 2001. The game went to sell 14.5 million copies.`, title:"Grand theft Auto 3", adjustment:"ps2standard"},
-    {text:`Another hit for the popular RPG series on the playstation platform released in 2001. The game went to sell 8.5 million copies worldwide.`, title:"Final Fantasy X", adjustment:"ps2standard"},
+    {text:`Another hit for the popular RPG series on the PlayStation platform released in 2001. The game went to sell 8.5 million copies worldwide.`, title:"Final Fantasy X", adjustment:"ps2standard"},
 ];
 export const ps3 =
 [
-    {text:`The start of the game series launched on playstation 3 in 2007. It sold 2.6 million copies.`, title:"Uncharted", adjustment:"ps3standard"},
-    {text:`The continuation of the hit game starting on the playstation 2. The sequel launched on playstation 3 in 2009. It sold 1 million copies worldwide.`, title:"Killzone 2", adjustment:"ps3standard"},
-    {text:`The start of the legendary series released on the playstation 3 in 2009. It went to sell 1 million copies worldwide.  `, title:"Demon Souls", adjustment:"ps3standard"},
+    {text:`The start of the game series launched on PlayStation 3 in 2007. It sold 2.6 million copies.`, title:"Uncharted", adjustment:"ps3standard"},
+    {text:`The continuation of the hit game starting on the PlayStation 2. The sequel launched on PlayStation 3 in 2009. It sold 1 million copies worldwide.`, title:"Killzone 2", adjustment:"ps3standard"},
+    {text:`The start of the legendary series released on the PlayStation 3 in 2009. It went to sell 1 million copies worldwide.  `, title:"Demon Souls", adjustment:"ps3standard"},
     {text:`This popular series released in 2009. It went to sell 2 million copies.`, title:"Infamous", adjustment:"ps3standard"},
     {text:`This popular series was released in 2008. It went to sell 3 million copies worldwide. `, title:"LittleBigPlanet", adjustment:"ps3standard"},
 ];
@@ -27,14 +27,14 @@ export const ps4 =
     {text:`A popular game series from the team that created dark souls. Released in 2015 it went to sell 2 million copies.`, title:"Bloodborne", adjustment:"ps4standard"},
     {text:`From the same team to create Ratchet and clank series is Spiderman. Released in 2018 it sold 13.2 million copies worldwide. `, title:"Marvel's Spider-man", adjustment:"ps4standard"},
     {text:`The sequel to the popular vita series released in 2017.`, title:"Gravity Rush 2", adjustment:"ps4standard"},
-    {text:`New popular game series makes its debut to the playstation 4 console. Released in 2020. It went to sell 5 million copies. `, title:"Ghost of Tsushima", adjustment:"ps4standard"},
-    {text:`New popular game series makes its debut to the playstation 4 console. Released in 2017. It went to sell 10 million copies. `, title:"Horizon Zero Dawn", adjustment:"ps4standard"},
+    {text:`New popular game series makes its debut to the PlayStation 4 console. Released in 2020. It went to sell 5 million copies. `, title:"Ghost of Tsushima", adjustment:"ps4standard"},
+    {text:`New popular game series makes its debut to the PlayStation 4 console. Released in 2017. It went to sell 10 million copies. `, title:"Horizon Zero Dawn", adjustment:"ps4standard"},
 ];
 
 export const psp=
 [
-    {text:`A popular game series that was on the playstation platform makes its way to psp. It was released in 2007.`, title: " Castlevania: Dracula X Chronicles", adjustment:"pspstandard", extra:"titleshrink" },
-    {text:`The popular fighting game series that its start on the playstation platform was on the psp handheld. It was released in 2006. `, title: "Guilty Gear Judgment", adjustment:"pspstandard",extra:"titleshrink" },
+    {text:`A popular game series that was on the PlayStation platform makes its way to psp. It was released in 2007.`, title: " Castlevania: Dracula X Chronicles", adjustment:"pspstandard", extra:"titleshrink" },
+    {text:`The popular fighting game series that its start on the PlayStation platform was on the psp handheld. It was released in 2006. `, title: "Guilty Gear Judgment", adjustment:"pspstandard",extra:"titleshrink" },
     {text:`The mega popular monster hunting series came to psp in 2007.`, title: "Monster Hunter Freedom 2", adjustment:"pspstandard",extra:"titleshrink" },
     {text:`The original series created for the psp system. Released in 2007.`, title: "Patapon", adjustment:"pspstandard" },
     {text:`The original series created for the psp system. Released in 2005.`, title: "Pursuit Force", adjustment:"pspstandard" }
@@ -42,7 +42,7 @@ export const psp=
 
 export const vita=
 [
-    {text:`A popular fighting game series starting in the original playstation. The fifth installment released for the vita in 2012.`, title: "Dead or Alive 5 Plus", adjustment:"vitastandard"},
+    {text:`A popular fighting game series starting in the original PlayStation. The fifth installment released for the vita in 2012.`, title: "Dead or Alive 5 Plus", adjustment:"vitastandard"},
     {text:`An original game title created for the vita. It released in 2014.`, title:"Freedom Wars", adjustment:"vitastandard"},
     {text:`A popular music puzzle game series starting on psp. The sequel released on the vita in 2011 `, title:"Lumines:Electronic Symphony", adjustment:"vitastandard"},
     {text:`The popular series that also was on the psp makes its way to vita.`, title:"Phantasy Star Nova", adjustment:"vitastandard"},
@@ -50,16 +50,16 @@ export const vita=
 ]
 export const vr=
 [
-    {text:`A new direction for the resident evil series. Use the playstation VR to experience survival horror in true immersion.`, title:"Resident Evil 7", adjustment:"vrstandard"},
-    {text:`Take flight in the latest long running franchise. Put on the Playstation VR and take to the skies. Released in 2019. `, title:"Ace Combat 7", adjustment:"vrstandard"},
-    {text:`Be the legendary batman using the playstation VR. Released in 2016.`, title:"Batman:Arkham VR", adjustment:"vrstandard"},
+    {text:`A new direction for the resident evil series. Use the PlayStation VR to experience survival horror in true immersion.`, title:"Resident Evil 7", adjustment:"vrstandard"},
+    {text:`Take flight in the latest long running franchise. Put on the PlayStation VR and take to the skies. Released in 2019. `, title:"Ace Combat 7", adjustment:"vrstandard"},
+    {text:`Be the legendary batman using the PlayStation VR. Released in 2016.`, title:"Batman:Arkham VR", adjustment:"vrstandard"},
     {text:`Play music in a fun way and in an immersive style. Released in 2019.`, title:"Beat Saber", adjustment:"vrstandard"},
-    {text:`A game where you can use the playstation VR to be a superbeing to save earth. Released in 2017.`, title:"Megaton Rainfall", adjustment:"vrstandard"},
+    {text:`A game where you can use the PlayStation VR to be a superbeing to save earth. Released in 2017.`, title:"Megaton Rainfall", adjustment:"vrstandard"},
 ]
 export const move=
 [
-    {text:`Use the playstation move to twist perspective to figure out puzzles. Released in 2010. `, title:"Echochrome 2", adjustment:"movestandard"},
-    {text:`Use the playstation move to help Ryu hack and slash into victory. Released in 2012.`, title:"Ninja Gaiden 3", adjustment:"movestandard"},
+    {text:`Use the PlayStation move to twist perspective to figure out puzzles. Released in 2010. `, title:"Echochrome 2", adjustment:"movestandard"},
+    {text:`Use the PlayStation move to help Ryu hack and slash into victory. Released in 2012.`, title:"Ninja Gaiden 3", adjustment:"movestandard"},
     {text:`Use the move in this classic rail shooting to shoot enemies using powerful weapons. Released in 2010. `, title:"Time Crisis: Razing Storm", adjustment:"movestandard", extra:"titleshrink"},
     {text:`Use the move to help the angry birds get their eggs. Released in 2012.`, title:"Angry Birds", adjustment:"movestandard"},
     {text:`Use the move to help Mickey to beat Mad Doctor. Released in 2012.`, title:"Epic Mickey 2: The Power of Two", adjustment:"movestandard", extra:"titleshrink"},
