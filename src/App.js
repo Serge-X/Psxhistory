06fff4bf2c18878df1a62082e956a740ac2d2598
Serge <lionheart.sergex@gmail.com>
2021-02-25 16:21:24 -0800

    changed routers

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import PSP from './ConsolePages/Playstation_Portable.js';
 import PsVita from './ConsolePages/Playstation_Vita.js';
 import PsMove from './ConsolePages/Playstation_Move.js';
 import PsVR from './ConsolePages/Playstation_VR.js';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {HashRouter, Route, Switch} from "react-router-dom";
 
 
 function App() {
   return (
     <div  >
-      <Router>
+      <HashRouter basename="/">
       <Switch>
-      <Route path="/Psxhistory/"  exact component={HomePage}/>
+      <Route path="/"  exact component={HomePage}/>
       <Route path="/Playstation1"   component={Playstation1}/>
       <Route path="/Playstation2"   component={Playstation2}/>
       <Route path="/Playstation3"   component={Playstation3}/>
@@ -27,7 +27,7 @@ function App() {
       <Route path="/PsMove"   component={PsMove}/>
       <Route path="/PsVR"   component={PsVR}/>
       </Switch>
-      </Router>
+      </HashRouter>
     </div>
 
 );
